fe9d776b6b55e343662998593277a594a43fa96a
RealRedFox <realredfox@mail.ru>
2026-02-27 13:54:55 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/TECNO/TECNO_LE7n
+LOCAL_PATH := device/tecno/le7n
 PRODUCT_PLATFORM := mt6768
 
 # V A/B
@@ -49,33 +49,36 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
 
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+# Health Hal - not needed for reduced recovery
+# PRODUCT_PACKAGES += \
+#    android.hardware.health@2.1-impl \
+#    android.hardware.health@2.1-service
 
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
+# Boot control HAL - not needed for reduced recovery
+# PRODUCT_PACKAGES += \
+#    android.hardware.boot@1.1-mtkimpl.recovery \
+#    android.hardware.boot@1.1-mtkimpl
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+# Excluded for reduced recovery
+# PRODUCT_PACKAGES_DEBUG += \
+#    bootctrl
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
+# Excluded for reduced recovery
+# PRODUCT_PACKAGES_DEBUG += \
+#    update_engine_client
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier 
+# Excluded for reduced recovery
+# PRODUCT_PACKAGES += \
+#    otapreopt_script \
+#    cppreopts.sh \
+#    update_engine \
+#    update_verifier 
 
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
