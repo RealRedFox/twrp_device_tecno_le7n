e29a5116100b224301549c9bb47d0f5d6ee66854
RealRedFox <realredfox@mail.ru>
2026-02-27 12:30:49 +0300

    Update twrp_tecno_le7n.mk

--- twrp_tecno_le7n.mk
+++ twrp_tecno_le7n.mk
@@ -32,7 +32,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/tecno/le7n/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := tecno_le7n
+PRODUCT_DEVICE := le7n
 PRODUCT_NAME := twrp_tecno_le7n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LE7n
