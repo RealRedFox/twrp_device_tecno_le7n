d6301d554646cf1e3412ca3e923b814a2c0f2df4
RealRedFox <realredfox@mail.ru>
2026-02-27 18:33:01 +0300

    Update pbrp_tecno_le7n.mk

--- pbrp_tecno_le7n.mk
+++ pbrp_tecno_le7n.mk
@@ -33,7 +33,7 @@ $(call inherit-product, device/tecno/le7n/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := le7n
-PRODUCT_NAME := twrp_tecno_le7n
+PRODUCT_NAME := pb_tecno_le7n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LE7n
 PRODUCT_MANUFACTURER := TECNO MOBILE LIMITED
